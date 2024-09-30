17038aca94290277452a9469600dd9f97a7aa61e
Aminul-Islam7 <aminulfardin7@gmail.com>
2024-10-01 01:08:17 +0600

    feat: Add text toggle for multiple cards

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 'use strict';
 
-// Selects the text paragraph and see more button
-const paragraph = document.getElementById('text');
-const btnSeeMore = document.getElementById('see-more');
+// Selects all the cards to loop over them
+const cards = document.querySelectorAll('.card');
+for (let card of cards) {
+	// Selects the text paragraph and see more button
+	const paragraph = card.querySelector('.text');
+	const btnSeeMore = card.querySelector('.see-more');
 
-const characterLimit = 100;
-const originalText = paragraph.innerText; // stores the whole text
-let shortenedText;
+	const characterLimit = 100;
+	const originalText = paragraph.innerText; // stores the whole text
+	let shortenedText;
 
-let toggleText = function () {
-	if (btnSeeMore.innerText === 'See more') {
-		// Expand text if collapsed and update button text
-		paragraph.innerText = originalText;
-		btnSeeMore.innerText = 'See less';
-	} else {
-		// Collapse text if expanded and update button text
+	let toggleText = function () {
+		if (btnSeeMore.innerText === 'See more') {
+			// Expand text if collapsed and update button text
+			paragraph.innerText = originalText;
+			btnSeeMore.innerText = 'See less';
+		} else {
+			// Collapse text if expanded and update button text
+			paragraph.innerText = shortenedText;
+			btnSeeMore.innerText = 'See more';
+		}
+	};
+
+	if (paragraph.innerText.length > 100) {
+		// Stores and displays first 'characterLimit' characters of the text
+		shortenedText = paragraph.innerText.substring(0, characterLimit) + '...';
 		paragraph.innerText = shortenedText;
-		btnSeeMore.innerText = 'See more';
+
+		btnSeeMore.style.display = 'block'; // show the see more button
 	}
-};
 
-if (paragraph.innerText.length > 100) {
-	// Store and display first 100 characters of the text
-	shortenedText = paragraph.innerText.substring(0, 99) + '...';
-	paragraph.innerText = shortenedText;
-
-	btnSeeMore.style.display = 'block'; // show the see more button
+	// See more button on click action
+	btnSeeMore.addEventListener('click', toggleText);
 }
-
-// See more button on click action
-btnSeeMore.addEventListener('click', toggleText);
